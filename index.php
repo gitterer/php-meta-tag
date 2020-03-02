3f7cd42e9ccc609a478f6876f17256792848d534
gitterer <tshiamojoseph@gmail.com>
2020-03-02 20:57:56 +0200

    create meta tag in files

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
-
-
-$sourcefile = "";
-$binary1 = fopen($sourcefile,'r');
-$size = filesize($sourcefile);
+//file directory
+$file = "volcanic.jpg";
+$binary = fopen($file,'r');
+$size = filesize($file);
 $hex = "";
+//ensure file is not empty
 if ($size < 1){
      die("Error");
 }
-   while (!feof($binary1)) {
-        $hexread = bin2hex(fread ($binary1 , 4 ));
+   while (!feof($binary)) {
+        $hexread = bin2hex(fread ($binary , 10 ));
          $hex = $hex.$hexread ;
     }
 
-fclose($binary1);
-echo $hex;
-
-    
-    
-   
-    
+$str = substr($hex, strlen($hex) - 4);
+if ($str <>  "ffd9"){
+     die("Cannot append meta data to file");
+}
+$meta = bin2hex("Meta data");
+$meta = $hex.$meta;
+strpos($meta," ");
+$bin = hex2bin($meta);   
+//write file to disk
+$file = fopen($file, 'w+');
+fwrite($file, $bin);
+fclose($file);
    
 ?>
